72e23ad36267a0ec3039805c097ebd5d9bc5757f
Natchanon <natchanon.347@gmail.com>
2019-12-26 03:46:11 +0700

    Initial Commit

---
+++ TrackableScrollViewUITests/TrackableScrollViewUITests.swift
@@ -0,0 +1,43 @@
+//
+//  TrackableScrollViewUITests.swift
+//  TrackableScrollViewUITests
+//
+//  Created by Natchanon A. on 26/12/2562 BE.
+//  Copyright © 2562 Natchanon A. All rights reserved.
+//
+
+import XCTest
+
+class TrackableScrollViewUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
