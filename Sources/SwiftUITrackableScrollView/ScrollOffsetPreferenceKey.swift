2d0f1d451112a5acc4834038eb718c0623297131
Mac <Mac>
2020-10-30 01:00:40 +0530

    SPM Package Created

--- Sources/SwiftUITrackableScrollView/ScrollOffsetPreferenceKey.swift
+++ Sources/SwiftUITrackableScrollView/ScrollOffsetPreferenceKey.swift
@@ -5,9 +5,9 @@
 //  Created by maxnatchanon on 26/12/2019 BE.
 //  Copyright © 2019 maxnatchanon All rights reserved.
 //
-
 import SwiftUI
 
+@available(iOS 13.0, *)
 struct ScrollOffsetPreferenceKey: PreferenceKey {
     typealias Value = [CGFloat]
     
