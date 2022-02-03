9187b6d748b5725800adb35ae5720ae86ca8a161
Matěj Volkmer <77694508+Matejvvo@users.noreply.github.com>
2022-02-03 15:58:37 +0100

    Update iOS support version to iOS 15

--- Sources/SwiftUITrackableScrollView/TrackableScrollView.swift
+++ Sources/SwiftUITrackableScrollView/TrackableScrollView.swift
@@ -7,7 +7,7 @@
 //
 import SwiftUI
 
-@available(iOS 13.0, *)
+@available(iOS 15.0, *)
 public struct TrackableScrollView<Content>: View where Content: View {
     let axes: Axis.Set
     let showIndicators: Bool
